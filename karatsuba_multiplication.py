a5eda7715057b3836bce5301303fd1e6504f6bd6
Daniel Joo <dsj2122@columbia.edu>
2020-08-29 16:17:55 -0700

    fixed the multiplier

--- karatsuba_multiplication.py
+++ karatsuba_multiplication.py
@@ -2,19 +2,27 @@
 import math
 
 class Multiplier:
-    def multiply(a,b):
-        n = len(str(a))
-        n2 = len(str(b))
-        if n ==1 and n2 ==1:
-            return a*b
-        a_half1 = math.floor(a/(10**(n/2)))
-        a_half2 = a%(10**(n/2))
-        b_half1 = math.floor(b/(10**(n/2)))
-        b_half2 = b%(10**(n/2))
-        ac = multiply(a_half1,b_half1)
-        bd = multiply(a_half2,b_half2)
-        sun = multiply((a_half1 + a_half2), (b_half1+b_half2)) - ac - bd
-        return (10**n)*ac + (10**(n/2))*sun + bd
+    def karatsuba(x,y):
+        if len(str(x)) == 1 or len(str(y)) == 1:
+            return x*y
+        else:
+            n = max(len(str(x)),len(str(y)))
+            nby2 = n // 2
+            
+            a = x // 10**(nby2)
+            b = x % 10**(nby2)
+            c = y // 10**(nby2)
+            d = y % 10**(nby2)
+            
+            ac = Multiplier.karatsuba(a,c)
+            bd = Multiplier.karatsuba(b,d)
+            ad_plus_bc = Multiplier.karatsuba(a+b,c+d) - ac - bd
+            
+                # this little trick, writing n as 2*nby2 takes care of both even and odd n
+            prod = ac * 10**(2*nby2) + (ad_plus_bc * 10**nby2) + bd
 
+            return prod
 
-print(Multiplier.multiply(3,5))
+number1 = 3141592653589793238462643383279502884197169399375105820974944592
+number2 = 2718281828459045235360287471352662497757247093699959574966967627
+print(Multiplier.karatsuba(number1,number2))
