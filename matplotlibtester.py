2ee3968ae8db685538bd0475de205cb4e937a526
Daniel Joo <dsj2122@columbia.edu>
2020-09-26 22:10:25 -0700

    made conda work

--- matplotlibtester.py
+++ matplotlibtester.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
-
 from matplotlib import pyplot as plt
+import matplotlib as npl
+import numpy as np
 
 x_values = [1,2,3,4]
 y_values = [5,6,7,8]
 
 plt.scatter(x_values, y_values)
 
-plt.show
+plt.show()
